c3fecf0aeb261ce219f05d3439a1d986d6776709
James <james.andrews23@gmail.com>
2021-02-23 15:36:13 -0600

    Basic camera movement
    Basic camera movement

---
+++ Assets/PerspectivePan.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerspectivePan : MonoBehaviour {
+    private Vector3 previousPos;
+    public Camera cam;
+    public float speed;
+    // Start is called before the first frame update
+
+    void Update(){
+        if (Input.GetMouseButtonDown(0)){
+            previousPos = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0)){
+            Vector3 direction = previousPos - cam.ScreenToViewportPoint(Input.mousePosition);
+            cam.transform.position += direction * speed;
+            previousPos = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+    }
+}
