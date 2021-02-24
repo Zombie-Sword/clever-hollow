c43b9d1f69272eca69bd92acabf0df1dfb3ea0a9
James <james.andrews23@gmail.com>
2021-02-24 14:35:58 -0600

    Adds zoom to camera script
    Adds zoom to camera script

--- Assets/PerspectivePan.cs
+++ Assets/PerspectivePan.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class PerspectivePan : MonoBehaviour {
     private Vector3 previousPos;
     public Camera cam;
-    public float speed;
+    public float speed = 20f;
+    private float targetZoom;
+    private float zoomFactor = 3f;
+    [SerializeField] private float zoomLerpSpeed = 10f;
+    public float minZoom = 8f;
+    public float maxZoom = 21.5f;
     // Start is called before the first frame update
+    private void Start()
+    {
+        targetZoom = cam.fieldOfView;
+    }
 
     void Update(){
         if (Input.GetMouseButtonDown(0)){
@@ -18,5 +27,12 @@ public class PerspectivePan : MonoBehaviour {
             cam.transform.position += direction * speed;
             previousPos = cam.ScreenToViewportPoint(Input.mousePosition);
         }
+
+        float scrollData;
+        scrollData = Input.GetAxis("Mouse ScrollWheel");
+
+        targetZoom -= scrollData * zoomFactor;
+        targetZoom = Mathf.Clamp(targetZoom, minZoom, maxZoom);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetZoom, Time.deltaTime * zoomLerpSpeed);
     }
 }
